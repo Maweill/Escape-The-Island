af53ccf4dc64882d2e7a1489b2896800f94f2186
Bednyakov <bednyakov99@gmail.com>
2023-01-29 19:37:11 +0300

    Исправил NRE в камере

--- Assets/_Project/Scripts/CameraFollow.cs
+++ Assets/_Project/Scripts/CameraFollow.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using _Project.Scripts.Factories;
 using UnityEngine;
+using Zenject;
 
 public class CameraFollow : MonoBehaviour
 {
+    [Inject]
+    private GameFactory _gameFactory = null!;
+    
     GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = _gameFactory.Player;
     }
 
     // Update is called once per frame
