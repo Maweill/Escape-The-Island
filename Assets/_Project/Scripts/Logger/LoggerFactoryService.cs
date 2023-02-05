f357ea58d33d1a1b20dc5fd3662cb8136b95192d
Bednyakov <bednyakov99@gmail.com>
2023-02-05 19:25:51 +0300

    Добавил кастомный логгер

---
+++ Assets/_Project/Scripts/Logger/LoggerFactoryService.cs
@@ -0,0 +1,20 @@
+﻿namespace _Project.Scripts.Logger
+{
+	public static class LoggerFactory
+	{
+		private const LoggerType LOGGER_TYPE = LoggerType.Custom;
+		
+		public static ICustomLogger GetLogger<T>()
+		{
+			switch (LOGGER_TYPE) {
+				case LoggerType.Custom:
+					return new CustomLogger(typeof(T));
+			}
+		}
+		
+		private enum LoggerType
+		{
+			Custom
+		}
+	}
+}
